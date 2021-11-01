a5f265c3438292bec9dd6f89b37ff3fc385a34eb
Noah Allan Ertz <nae@NAE-AGD0-GA000-DMACC.localdomain>
2021-11-01 16:26:20 -0500

    Combined the Windows and *Nix versions for the colored text

--- Game/print.h
+++ Game/print.h
@@ -1,6 +1,14 @@
 #ifndef PRINT_H
 #define PRINT_H
 
+#if defined(_WIN32) || defined(_WIN64)
+#define WINDOWS
+#elif defined(__unix__) || defined(__APPLE__) || defined(__MACH__)
+#define NIX
+#else
+#define UNKNOWN
+#endif // OS Definitions
+
 #include <string>
 
 enum text_color { BLACK, DARK_RED, DARK_GREEN, DARK_YELLOW, DARK_BLUE, DARK_MAGENTA, DARK_CYAN, DARK_WHITE, GRAY, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE };
